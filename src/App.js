6631ea40adfadd630688f73c4e3384c104240a18
mateuszmeme <matsawka@yahoo.com>
2021-11-18 10:52:55 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Carousel, Text, Button, Box } from 'grommet';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">      
+      <Box align="center" pad="large">
+    <Carousel controls="arrows" height="large" fill>
+      <Box
+        fill
+        align="center"
+        justify="center"
+        background="lavender"
+        gap="small"
+      >
+        <Text weight="bold" size="xlarge">
+          Slide 1
+        </Text>
+        <Button label="Button" />
+      </Box>
+      <Box
+        fill
+        align="center"
+        justify="center"
+        background="light-3"
+        gap="small"
+      >
+        <Text weight="bold" size="xlarge">
+          Slide 2
+        </Text>
+        <Button label="Button" />
+        <Button label="Button" />
+      </Box>
+      <Box
+        fill
+        align="center"
+        justify="center"
+        background="light-5"
+        gap="small"
+      >
+        <Text weight="bold" size="xlarge">
+          Slide 3
+        </Text>
+        <Button label="Button" />
+        <Button label="Button" />
+        <Button label="Button" />
+      </Box>
+    </Carousel>
+  </Box>
     </div>
   );
 }
